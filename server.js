664cc924ae1f80cc7a63fa9c67f29c7d6b0b0b9a
turki7889 <trki7889@gmail.com>
2026-02-25 16:50:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -104,6 +104,6 @@ app.post('/webhook', async (req, res) => {
 // Start Server
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
 app.listen(port, () => console.log(`Server running on port ${port}`));
